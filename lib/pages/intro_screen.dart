a6f35c2045b6da4017bbeac150f0053309200386
TheDOme6 <dominik2003schulze@gmail.com>
2020-06-11 18:34:35 +0200

    svg changes

--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class IntroScreen extends StatefulWidget {
   IntroScreen({Key key}) : super(key: key);
@@ -11,7 +12,7 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Test'),
+      child: SvgPicture.asset('assets/intro_screens/intro_screen_1.svg'),
     );
   }
 }
