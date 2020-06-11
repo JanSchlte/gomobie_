0a9db96e4b981216d05ad3d455c1104076d937fe
TheDOme6 <dominik2003schulze@gmail.com>
2020-06-11 20:40:31 +0200

    finishup intro-screen

--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -11,7 +11,18 @@ class IntroScreen extends StatefulWidget {
 }
 
 class _IntroScreenState extends State<IntroScreen> {
+  double _currentPage = 0;
   final PageController _controller = PageController(viewportFraction: 0.99);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      setState(() {
+        _currentPage = _controller.page;
+      });
+    });
+  }
 
   Widget _buildPageIndicator() {
     return SizedBox(
@@ -19,9 +30,11 @@ class _IntroScreenState extends State<IntroScreen> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          _singleIndicator(_controller.page < 1 ? true : false),
-          _singleIndicator(_controller.page < 2 ? true : false),
-          _singleIndicator(_controller.page < 3 ? true : false),
+          _singleIndicator(_currentPage < 1 ? true : false),
+          _singleIndicator(
+              _currentPage < 2 && _currentPage >= 1 ? true : false),
+          _singleIndicator(
+              _currentPage < 3 && _currentPage >= 2 ? true : false),
         ],
       ),
     );
@@ -38,71 +51,133 @@ class _IntroScreenState extends State<IntroScreen> {
     );
   }
 
+  static Widget _buildLoads(String first, String last) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        text: first,
+        style: TextStyle(fontSize: 23, color: Color(0xFF1ABC9C)),
+        children: [
+          TextSpan(
+            text: last,
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.grey,
+              height: 2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static Widget _buildBulletPoint(String first, String last) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        text: first,
+        style: TextStyle(
+          fontSize: 15,
+          color: Colors.grey,
+          height: 1.2,
+        ),
+        children: [
+          TextSpan(
+            text: last,
+            style: TextStyle(fontSize: 23, color: Color(0xFF1ABC9C)),
+          ),
+        ],
+      ),
+    );
+  }
+
   final List<Widget> _screens = [
     FirstScreen(
       imageAsset: 'assets/intro_screens/intro_screen_1.png',
-      firstText: '95%',
-      lastText: ' aller Teenager besitzen ein\n'
-          'Smartphone, aber fast niemand\n'
-          'von ihnen kann selbst online bezahlen',
+      text: <Widget>[
+        _buildLoads(
+            '95%',
+            ' aller Teenager besitzen ein\n'
+                'Smartphone, aber fast niemand\n'
+                'von ihnen kann selbst online bezahlen')
+      ],
     ),
     FirstScreen(
       imageAsset: 'assets/intro_screens/intro_screen_2.png',
-      firstText: 'Gomobie',
-      lastText: ' macht es möglich,\n'
-          'dass Kinder und Jugendliche unabhängig\n'
-          'und sicher im Internet einkaufen können',
+      text: <Widget>[
+        _buildLoads(
+            'Gomobie',
+            ' macht es möglich,\n'
+                'dass Kinder und Jugendliche unabhängig\n'
+                'und sicher im Internet einkaufen können')
+      ],
     ),
-    FirstScreen(),
+    FirstScreen(
+      imageAsset: 'assets/intro_screens/intro_screen_3.png',
+      text: <Widget>[
+        _buildBulletPoint('Umfassende Einstellungen für\n', 'Eltern'),
+        SizedBox(height: 10),
+        _buildBulletPoint(
+            'Angepasst auf spezielle'
+                ' Bedürfnisse\nund Wünsche der\n',
+            'Kinder'),
+        SizedBox(height: 10),
+        _buildBulletPoint('Kostenloser\n', 'Schutz')
+      ],
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            PageView(
-              controller: _controller,
-              children: _screens,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            _buildPageIndicator(),
-            SizedBox(
-              height: 30,
-            ),
-            RaisedButton(
-              onPressed: () {
-                _controller.nextPage(
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.easeIn);
-                setState(() {});
-              },
-              child: Text('WEITER'),
-              textColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6)),
-              elevation: 8,
-              padding: EdgeInsets.symmetric(horizontal: 130, vertical: 20),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            FlatButton(
-              onPressed: () {},
-              child: Text(
-                'ÜBERSPRINGEN',
-                style: TextStyle(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 17),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: PageView.builder(
+                  controller: _controller,
+                  itemBuilder: (context, index) => _screens[index],
+                  itemCount: _screens.length,
+                ),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 90, vertical: 20),
-            )
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              _buildPageIndicator(),
+              SizedBox(
+                height: 30,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _controller.nextPage(
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeIn);
+                },
+                child: Text('WEITER'),
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6)),
+                elevation: 8,
+                padding: EdgeInsets.symmetric(horizontal: 130, vertical: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'ÜBERSPRINGEN',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 90, vertical: 20),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/intro_screen/first_screen.dart
+++ lib/pages/intro_screen/first_screen.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget {
   final String imageAsset;
-  final String firstText;
-  final String lastText;
+  final List<Widget> text;
 
-  const FirstScreen({Key key, this.imageAsset, this.firstText, this.lastText}) : super(key: key);
+  const FirstScreen({Key key, this.imageAsset, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,23 +12,7 @@ class FirstScreen extends StatelessWidget {
       children: <Widget>[
         Image.asset(imageAsset),
         SizedBox(height: 30),
-        RichText(
-          textAlign: TextAlign.center,
-          text: TextSpan(
-            text: firstText,
-            style: TextStyle(fontSize: 23, color: Color(0xFF1ABC9C)),
-            children: [
-              TextSpan(
-                text: lastText,
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
-                  height: 2,
-                ),
-              ),
-            ],
-          ),
-        )
+        ...text
       ],
     );
   }
