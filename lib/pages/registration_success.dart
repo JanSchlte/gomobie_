34667e9674bcf797b5e399bb52ad02abc5e63d1c
Jan <jan.schulte0211@gmail.com>
2020-06-12 00:05:46 +0200

    Registrierung_Successful Screen hinzugefügt. Bedarf noch einiger Design Optimierung (v. A. Logo-Auflösung und Abstand beim InfoText)

---
+++ lib/pages/registration_success.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class RegistrationSuccess extends StatelessWidget {
+  static const routeName = '/success';
+
+  Widget _buildContinueButton() {
+    return RaisedButton(
+      onPressed: () {
+        //Navigator.of(context).pushNamed(home-route.routeName);
+        //TODO: Insert Home-Route
+      },
+      child: Text('FERTIG'),
+      textColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      elevation: 8,
+      padding: EdgeInsets.symmetric(horizontal: 137, vertical: 20),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/registration_screens/registration_successful.png',
+                height: 150,
+                //TODO: Higher resolution
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Registrierung erfolreich!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Bitte klicken Sie auf den \n'
+                'Bestätigungslink in Ihrer Mail.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 150.0,
+              ),
+              _buildContinueButton(),
+              SizedBox(
+                height: 50.0,
+              ),
+              Text(
+                'Weitere Einstellungen können Sie über das',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey),
+              ),
+              Row(
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      //TODO: Insert Home-Route
+                      //Navigator.of(context).pushNamed(home-route.routeName);
+                    },
+                    //TODO: Abstand fixen
+                    child: Text(
+                      'Verwaltungskonto',
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  Text(
+                    'durchführen',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
