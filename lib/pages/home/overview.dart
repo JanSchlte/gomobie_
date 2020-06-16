923a972220cf5d087c9da4593c415edc42d5a15d
Jan <jan.schulte0211@gmail.com>
2020-06-16 22:43:43 +0200

    SingleChildScroll

--- lib/pages/home/overview.dart
+++ lib/pages/home/overview.dart
@@ -15,104 +15,102 @@ class Overview extends StatelessWidget {
         child: Material(
           color: Colors.grey.shade800.withOpacity(0.75),
           child: SafeArea(
-            child: SingleChildScrollView(
-              child: Center(
-                child: provider.hasData
-                    ? Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 50,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.add),
-                                  onPressed: () {
-                                    //TODO: Add upload image page
-                                  },
-                                ),
-                                Text(
-                                  'Bild hinzufügen',
-                                  style: Theme.of(context).textTheme.caption,
-                                ),
-                              ],
-                            ),
-                            backgroundColor: Colors.white,
-                          ),
-                          Text(provider.userData.name),
-                          Card(
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    CircleAvatar(
-                                        child: Icon(Icons.call_received)),
-                                    CircleAvatar(child: Icon(Icons.send)),
-                                    CircleAvatar(child: Icon(Icons.group)),
-                                  ],
-                                ),
-                                Text(
-                                    '${(provider.userData.balance / 100).toStringAsFixed(2)}€'),
-                              ],
-                            ),
-                          ),
-                          Wrap(
+            child: Center(
+              child: provider.hasData
+                  ? Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
                             children: [
-                              Card(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text('Sammelgruppe 1'),
-                                    Container(
-                                      height: 70,
-                                      width: 70,
-                                      margin: EdgeInsets.all(30),
-                                      child: Stack(
-                                        fit: StackFit.expand,
-                                        children: [
-                                          CircularProgressIndicator(
-                                            value: 0.8,
-                                            strokeWidth: 6,
-                                          ),
-                                          Align(
-                                            alignment: Alignment.center,
-                                            child: Column(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                Text('80.00€'),
-                                                Text('100.00€'),
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                              IconButton(
+                                icon: Icon(Icons.add),
+                                onPressed: () {
+                                  //TODO: Add upload image page
+                                },
                               ),
-                              Card(
-                                child: Container(
-                                  height: 145,
-                                  width: 130,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text('Offene\nRechnungen'),
-                                      Text('-34.99€'),
-                                      SizedBox()
-                                    ],
-                                  ),
-                                ),
+                              Text(
+                                'Bild hinzufügen',
+                                style: Theme.of(context).textTheme.caption,
                               ),
                             ],
-                          )
-                        ],
-                      )
-                    : Text('LOADING...'),
-              ),
+                          ),
+                          backgroundColor: Colors.white,
+                        ),
+                        Text(provider.userData.name),
+                        Card(
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  CircleAvatar(
+                                      child: Icon(Icons.call_received)),
+                                  CircleAvatar(child: Icon(Icons.send)),
+                                  CircleAvatar(child: Icon(Icons.group)),
+                                ],
+                              ),
+                              Text(
+                                  '${(provider.userData.balance / 100).toStringAsFixed(2)}€'),
+                            ],
+                          ),
+                        ),
+                        Wrap(
+                          children: [
+                            Card(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text('Sammelgruppe 1'),
+                                  Container(
+                                    height: 70,
+                                    width: 70,
+                                    margin: EdgeInsets.all(30),
+                                    child: Stack(
+                                      fit: StackFit.expand,
+                                      children: [
+                                        CircularProgressIndicator(
+                                          value: 0.8,
+                                          strokeWidth: 6,
+                                        ),
+                                        Align(
+                                          alignment: Alignment.center,
+                                          child: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Text('80.00€'),
+                                              Text('100.00€'),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Card(
+                              child: Container(
+                                height: 145,
+                                width: 130,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Offene\nRechnungen'),
+                                    Text('-34.99€'),
+                                    SizedBox()
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  : Text('LOADING...'),
             ),
           ),
         ));
