ccc1600bc8d64d41fd100efd84f8c86e01dc486e
Jan <jan.schulte0211@gmail.com>
2020-06-21 09:17:15 +0200

    Improvement of the add_family_member alertdialog in terms of the design. Backend functionality must still be added

--- lib/pages/home/family.dart
+++ lib/pages/home/family.dart
@@ -82,7 +82,7 @@ class Family extends StatelessWidget {
                 labelText: 'Benutzer ID',
                 labelStyle:
                     TextStyle(color: Colors.grey.shade500, fontSize: 13),
-                //TODO: Avoid Bottom Overflow despite usage of SingelChildScrollView
+                //TODO: Avoid Bottom Overflow despite usage of SingelChildScrollView and resizeToAvoidBottomOverflow = true (Scaffold)
               ),
             ),
           ],
@@ -116,60 +116,63 @@ class Family extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Image.asset(
-              'assets/general_assets/family.png',
-              height: 170,
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Familie',
-              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
-            ),
-            Text('Familienmitglieder'),
-            SizedBox(height: 50),
-            Stack(
-              fit: StackFit.loose,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 16, right: 16, bottom: 35),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    elevation: 3,
-                    child: Column(
-                      children: [
-                        //TODO: Fetch from firebase
-                        FamilyCard(
-                            name: 'Tobias Henning', type: AccountType.owner),
-                        FamilyCard(
-                            name: 'Emma Henning', type: AccountType.owner),
-                        FamilyCard(
-                            name: 'Mattis Henning', type: AccountType.child),
-                      ],
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Image.asset(
+                'assets/general_assets/family.png',
+                height: 170,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Familie',
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
+              ),
+              Text('Familienmitglieder'),
+              SizedBox(height: 50),
+              Stack(
+                fit: StackFit.loose,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, bottom: 35),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      elevation: 3,
+                      child: Column(
+                        children: [
+                          //TODO: Fetch from firebase
+                          FamilyCard(
+                              name: 'Tobias Henning', type: AccountType.owner),
+                          FamilyCard(
+                              name: 'Emma Henning', type: AccountType.owner),
+                          FamilyCard(
+                              name: 'Mattis Henning', type: AccountType.child),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: FloatingActionButton.extended(
-                      onPressed: () {
-                        showAlertDialog(context);
-                      },
-                      label: Text('Hinzufügen'),
-                      icon: Icon(Icons.add),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          showAlertDialog(context);
+                        },
+                        label: Text('Hinzufügen'),
+                        icon: Icon(Icons.add),
+                      ),
                     ),
-                  ),
-                )
-              ],
-            )
-          ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
